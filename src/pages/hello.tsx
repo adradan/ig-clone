a26685ac4b1373f64423f189bb82703b52014ec7
adradan <ericvsolcruz@gmail.com>
2022-09-01 16:28:16 -0400

    fixed aspect ratio/image caching

--- src/pages/hello.tsx
+++ src/pages/hello.tsx
@@ -3,7 +3,7 @@ import { useRef, useState } from 'react';
 
 import { Meta } from '@/layouts/Meta';
 import { Main } from '@/templates/Main';
-
+// Work on dragging image
 const Hello = () => {
   const [image, setImage] = useState(undefined as unknown);
   const [err, setErr] = useState('');
@@ -14,6 +14,7 @@ const Hello = () => {
   const [imgUrl, setUrl] = useState('');
   const [checked, setChecked] = useState(0);
   const imgParent = useRef<null | HTMLDivElement>(null);
+  const imgDiv = useRef<null | HTMLDivElement>(null);
 
   const getSquare = () => {
     // if (!imgParent.current) return;
@@ -23,49 +24,37 @@ const Hello = () => {
     setNewW(800);
   };
 
+  const imgLoad = () => {
+    console.log('loaded');
+    if (!imgDiv.current) return;
+    const { current } = imgDiv;
+    current.style.backgroundImage = `url(${imgUrl})`;
+
+    const img = new Image();
+    img.onload = () => {
+      const { height, width } = img;
+      console.log(height, width);
+      setH(height);
+      setW(width);
+    };
+    img.src = imgUrl;
+  };
+
   const handleImgChange = (event: ChangeEvent<HTMLInputElement>) => {
     const imgFile = event.target.files ? event.target.files[0] : undefined;
     if (!imgFile) {
       setErr('Invalid File.');
       return;
     }
-    if (!imgFile?.name.match(/\.(jpeg|jpg|png)$/g)) {
+    if (!imgFile?.name.toLowerCase().match(/\.(jpeg|jpg|png)$/g)) {
       setErr('Invalid File Type.');
       return;
     }
     setErr('');
-    const newImage = new Image();
     const wURL = window.URL || window.webkitURL;
-    // Find fix for cache
-    // Unreliable image load
-    // revokeObjectURL?
     const url = wURL.createObjectURL(imgFile);
     setUrl(url);
-
-    console.log(newImage.height, newImage.width);
     console.log(image);
-    newImage.onload = () => {
-      const { height, width } = newImage;
-      setH(height);
-      setW(width);
-      console.log('asdf');
-      console.log(height, width);
-      getSquare();
-      setChecked(1);
-    };
-
-    newImage.src = imgUrl;
-    // newImage.onload = () => {
-    //   const { height, width } = newImage;
-    //   if (height > 1350 || width > 1080) {
-    //     console.log('Too large.');
-    //   }
-    //   setH(String(height));
-    //   setW(String(width));
-
-    //   // canvasElem.height = height;
-    //   // canvasElem.width = width;
-    // };
     setImage(imgFile);
   };
 
@@ -108,15 +97,48 @@ const Hello = () => {
     console.log(longSide);
   };
 
+  const getFourFive = (): void => {
+    if (!imgParent.current) return;
+    const { current } = imgParent;
+    const rect = current.getBoundingClientRect();
+    const { height, width } = rect;
+    // 4:5 ratio is .8
+    const newWidth = width * 0.8;
+    setNewW(newWidth);
+    setNewH(height);
+  };
+
+  const getSixteen = (): void => {
+    if (!imgParent.current) return;
+    const { current } = imgParent;
+    const rect = current.getBoundingClientRect();
+    const { height, width } = rect;
+    // 16:9 ratio is .5625
+    const newHeight = height * 0.5625;
+    setNewH(newHeight);
+    setNewW(width);
+  };
+
   const aspectChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { target } = event;
     const { value } = target;
     const numVal = Number(value);
     setChecked(numVal);
-    if (numVal === 0) {
-      getOriginal();
-    } else if (numVal === 1) {
-      getSquare();
+    switch (numVal) {
+      case 0:
+        getOriginal();
+        break;
+      case 1:
+        getSquare();
+        break;
+      case 2:
+        getFourFive();
+        break;
+      case 3:
+        getSixteen();
+        break;
+      default:
+        break;
     }
   };
 
@@ -180,23 +202,28 @@ const Hello = () => {
           </div>
         </div>
         {/* Figure out how to resize image depending on aspect ratio */}
+        <div style={{ display: 'none' }}>
+          <img src={imgUrl} onLoad={imgLoad} alt="Hidden Image" />
+        </div>
         <div
           ref={imgParent}
           className="flex aspect-square w-800 items-center justify-center"
         >
           <div
             style={{
-              backgroundImage: `url(${imgUrl})`,
               width: `${newW || 800}px`,
-              // height: `${newH || 800}px`,
+              height: `${newH || 800}px`,
             }}
             className="h-full w-full bg-cover bg-center bg-no-repeat"
+            ref={imgDiv}
           />
         </div>
         <div className="flex aspect-square w-800 justify-around">
           <div
-            style={{ backgroundImage: `url(${imgUrl})` }}
             className="h-full w-full bg-contain bg-center bg-no-repeat"
+            style={{
+              backgroundImage: `url(${imgUrl})`,
+            }}
           />
         </div>
       </div>
